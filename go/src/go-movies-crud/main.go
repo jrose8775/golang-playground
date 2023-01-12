b3ad7c4d8271011b6415beceff12e52fd5146a4f
Jordan Rose <jrose8775@gmail.com>
2023-01-12 17:49:56 -0400

    get movie and movies delete movie

--- go/src/go-movies-crud/main.go
+++ go/src/go-movies-crud/main.go
@@ -2,6 +2,15 @@
 // CRUD application
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
 	ID       string    `json: "id"`
 	Isbn     string    `json: "isbn"`
@@ -12,4 +21,59 @@ type Movie struct {
 type Director struct {
 	Firstname string `json: "firstname"`
 	Lastname  string `json: "lastname"`
+}
+
+// Slice for movies
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// Hardcode in a couple movies
+	movies = append(movies, Movie{
+		ID:       "1",
+		Isbn:     "1000-2412-231",
+		Title:    "Die Hard",
+		Director: &Director{Firstname: "Tom", Lastname: "Hardy"}})
+	movies = append(movies, Movie{
+		ID:       "2",
+		Isbn:     "3000-2112-231",
+		Title:    "Alien",
+		Director: &Director{Firstname: "Susan", Lastname: "Bill"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	// r.HandleFunc("/movies", createMovie).Methods("POST")
+	// r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting Server at Port 8000\n")
+	log.Fatal(http.ListenAndServe("8000", r))
 }
