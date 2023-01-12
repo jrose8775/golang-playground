ce6798efe7170e1f8f1a379e2bd831c4bc9f2159
Jordan Rose <jrose8775@gmail.com>
2023-01-12 17:19:54 -0400

    repo mods

---
+++ go/src/go-movies-crud/main.go
@@ -0,0 +1,15 @@
+// struct and slices
+// CRUD application
+package main
+
+type Movie struct {
+	ID       string    `json: "id"`
+	Isbn     string    `json: "isbn"`
+	Title    string    `json: "title"`
+	Director *Director `json: "director"`
+}
+
+type Director struct {
+	Firstname string `json: "firstname"`
+	Lastname  string `json: "lastname"`
+}
